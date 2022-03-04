89a0f13a606e1977dd75fa032c5b90a17405f6d2
Renato Felipe <rfelipesp@gmail.com>
2022-03-04 18:31:20 -0300

    Initial commit, Methods POST, GET, PUT, DELETE Add Pageable and Date Config

---
+++ src/main/java/com/api/parkingcontrol/service/ParkingSpotService.java
@@ -0,0 +1,56 @@
+package com.api.parkingcontrol.service;
+
+import com.api.parkingcontrol.model.ParkingSpotModel;
+import com.api.parkingcontrol.repository.ParkingSpotRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class ParkingSpotService {
+
+    final
+    ParkingSpotRepository parkingSpotRepository;
+
+    public ParkingSpotService(ParkingSpotRepository parkingSpotRepository) {
+        this.parkingSpotRepository = parkingSpotRepository;
+    }
+
+    @Transactional
+    public ParkingSpotModel save(ParkingSpotModel parkingSpotModel) {
+        return parkingSpotRepository.save(parkingSpotModel);
+    }
+
+    public boolean existsByLicensePlateCar(String licensePlateCar) {
+        return parkingSpotRepository.existsByLicensePlateCar(licensePlateCar);
+    }
+
+    public boolean existsByParkingSpotNumber(String parkingSpotNumber) {
+        return parkingSpotRepository.existsByParkingSpotNumber(parkingSpotNumber);
+    }
+
+    public boolean existsByApartmentAndBlock(String apartament, String block) {
+        return parkingSpotRepository.existsByApartmentAndBlock(apartament, block);
+    }
+
+    public Page<ParkingSpotModel> finfAll(Pageable pageable) {
+        return parkingSpotRepository.findAll(pageable);
+    }
+
+    public Optional<ParkingSpotModel> findById(UUID id) {
+        return parkingSpotRepository.findById(id);
+    }
+
+    @Transactional
+    public void delete(ParkingSpotModel parkingSpotModel) {
+        parkingSpotRepository.delete(parkingSpotModel);
+    }
+
+    public void update(ParkingSpotModel parkingSpotModel) {
+    }
+}
